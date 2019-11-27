36429bb43a51de439c371d8533953c764611e3a0
abiliquip <ryan@abiliquip.com>
2019-11-28 09:39:54 +1300

    started motor control

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
  * adc pi, abelectronics.co.uk for voltage readings
  * Havnt got current baord yet
  * 
+ * Authour: Ryan Taylor
+ * 
  * */
 
 #include <stdio.h>
@@ -15,6 +17,20 @@
 #define RELAY3 0xfd
 #define RELAY4 0xf7
 #define RELAYOFF 0xff
+
+void init(void){
+	bcm2835_init();
+    bcm2835_i2c_begin();                //Start I2C operations.
+    bcm2835_i2c_setSlaveAddress(0x20);  //I2C address
+    bcm2835_i2c_set_baudrate(10000);    //1M baudrate
+	
+}
+
+void deinit(void){
+	bcm2835_i2c_end();  
+    bcm2835_close();  
+	
+}
 
 int select_relay(int relay){
 	if (relay == 1){
@@ -38,26 +54,45 @@ void relay_control(bool state, int relay){
 	int control = select_relay(relay);
 	
 	if (state == 1){
-		buf[0] = 0x06;	//Address
+		buf[0] = 0x06;		//Address
         buf[1] = control;	//DATA
 	}
 	else if (state == 0){
 		buf[0] = 0x06;     
-        buf[1] = RELAYOFF; //LED OFF
+        buf[1] = RELAYOFF; //OFF
 	}
 	bcm2835_i2c_write(buf,2);
 }
 
+void position_feedback(){}
+void act_control(int active_act, int direction){
+	if(direction == 1){
+		relay_control(1,1);
+		
+	}
+	else if(direction == 2){
+		
+		
+	}
+	else{
+		relay_control(0, 1);
+		relay_control(0, 2);
+		relay_control(0, 3);
+		relay_control(0, 4);
+	}
+}
 
-int main(int argc, char **argv)  
-{  
 
-    if (!bcm2835_init())return 1;  
-    bcm2835_i2c_begin();                //Start I2C operations.
-    bcm2835_i2c_setSlaveAddress(0x20);  //I2C address
-    bcm2835_i2c_set_baudrate(10000);    //1M baudrate
+int main(int argc, char **argv)  {  
+	init();
+
     int count = 0;
+    int active_act = 1;
+    int direction = 0;
     while(1)  {   
+		position_feedback();
+		act_control(active_act, direction);
+		
 		relay_control(1,count);
 		delay(500);
 		relay_control(0,count);
@@ -68,7 +103,6 @@ int main(int argc, char **argv)
 		}
 		count++;
     }    
-    bcm2835_i2c_end();  
-    bcm2835_close();  
+	deinit();
     return 0;  
 } 
